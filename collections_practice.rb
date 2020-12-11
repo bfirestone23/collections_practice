0be940b288fe2e2accfb1c1a5e4a257002756c66
Brian Firestone <bfirestone2339@gmail.com>
2020-12-10 19:45:18 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |word|
+        word[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |word|
+        word.start_with?('a')
+    end
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array_with_s = []
+    array.each do |word|
+        if word == array[1]
+            array[1]
+        else
+            array_with_s << word.concat("s")
+        end
+    end
+end
